1f135bde4de02b5bdff604b1889de4593cab92aa
niteshjangid29 <niteshjangidofficial@gmail.com>
2022-07-14 06:06:52 +0530

    Responsive Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
